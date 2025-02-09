30e23eb830da15f6ac994af7e29e8774f8cca93e
s31237737 <s1237737@nate.com>
2025-02-09 21:58:18 +0900

    header 작업중

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,6 +11,25 @@
       />
     </template>
     <template #append>
+      <div class="login-session">
+        <span class="timer">
+          <v-icon
+            size="32"
+          >
+            custom:clock
+          </v-icon>
+          {{ timeLeftFormatted }}
+        </span>
+        <v-btn
+          color="white"
+          size="x-small"
+          variant="outlined"
+          @click="onExtendClick"
+        >
+          연장
+        </v-btn>
+      </div>
+
       <v-btn
         icon
         size="32"
@@ -50,10 +69,33 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
 // 알림 상태 (알림유무로 true/false로 설정)
 const hasNotification = ref(true);
+
+
+// 남은 시간을 초 단위로 관리
+const remainingTime = ref(30 * 60);
+// 남은 시간을 "분:초" 형식으로 반환하는 계산된 속성
+const timeLeftFormatted = computed(() => {
+  const minutes = Math.floor(remainingTime.value / 60);
+  const seconds = remainingTime.value % 60;
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+});
+
+onMounted(() => {
+  setInterval(() => {
+    if (remainingTime.value > 0) {
+      remainingTime.value -= 1;
+    }
+  }, 1000); // 1초마다 갱신
+});
+
+// 시간 연장
+const onExtendClick = () => {
+  remainingTime.value += 30 * 60;
+};
 </script>
 
 <style scoped lang="sass">
