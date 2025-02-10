613c5c56c14c936ea141774096c1aa4862d97203
lim <95602250+s31237737@users.noreply.github.com>
2025-02-10 17:30:45 +0900

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,42 @@
 <template>
-  <p class="t1">
-    typography
-  </p>
-  <p class="t2">
-    typography
-  </p>
-  <p class="p1 bold">
-    typography
-  </p>
-  <p class="p1 semibold">
-    typography
-  </p>
-  <p class="p2">
-    typography
-  </p>
-  <p class="p2 regular">
-    typography
-  </p>
-  <p class="c1">
-    typography
-  </p>
+  <v-row>
+    <v-col>
+      <h1>
+        Title 1 (T1)
+      </h1>
+      <p class="body-1">
+        This is a bold paragraph using body1 style (16px, 150% line height).
+      </p>
+      <h2>
+        Title 2 (T2)
+      </h2>
+      <p class="body-2">
+        This is a semi-bold paragraph using body2 style (16px, 150% line height).
+      </p>
+    </v-col>
+  </v-row>
+
+  <v-row>
+    <v-col>
+      <p class="body-1">
+        P1 - Bold Paragraph
+      </p>
+      <p class="body-2">
+        P2 - SemiBold Paragraph
+      </p>
+      <p class="body-3">
+        P3 - Regular Paragraph
+      </p>
+    </v-col>
+  </v-row>
+
+  <v-row>
+    <v-col>
+      <p class="caption">
+        Caption 1 (C1) - Regular Caption
+      </p>
+    </v-col>
+  </v-row>
 
   <v-icon
     size="32"
@@ -41,7 +58,6 @@
       <v-btn
         size="x-small"
         color="primary"
-        flat
       >
         Extra small Button
       </v-btn>
@@ -51,7 +67,6 @@
       <v-btn
         size="small"
         color="red"
-        flat
         variant="outlined"
       >
         Small Button
@@ -65,7 +80,7 @@
     </v-col>
 
     <v-col cols="auto">
-      <v-btn size="large">
+      <v-btn size="large" color="state_warn">
         Large Button
       </v-btn>
     </v-col>
