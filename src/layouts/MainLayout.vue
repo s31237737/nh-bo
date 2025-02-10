9f6d923fc33a87d75e1d130cc61b3f9d8b5f1381
lim <95602250+s31237737@users.noreply.github.com>
2025-02-10 14:27:52 +0900

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,12 +2,28 @@
   <Header />
   <Lnb />
   <v-main>
-    <slot />
+    <v-container fluid>
+      <!-- loading persistent속성 추가 필요-->
+      <v-overlay
+        :model-value="overlay"
+        contained
+      >
+        <v-progress-circular
+          :size="35"
+          :width="5"
+          color="white"
+          indeterminate
+        />
+      </v-overlay>
+      <!-- // loading -->
+      <slot />
+    </v-container>
   </v-main>
 </template>
 
 <script setup>
-import {  } from "module";
+import { ref } from "vue";
+const overlay = ref(true);
 </script>
 
 <style scoped>
