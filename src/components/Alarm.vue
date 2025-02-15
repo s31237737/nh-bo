1d36e8f8dda847c06564ed67125cebf0db5693a2
s31237737 <s1237737@nate.com>
2025-02-15 19:35:28 +0900

    알림 완료

--- src/components/Alarm.vue
+++ src/components/Alarm.vue
@@ -33,7 +33,7 @@
             <v-btn
               size="24"
               icon="custom:arrow-left"
-              @click="isSetting=!isSetting"
+              @click="toggleSetting"
             />
             <v-toolbar-title
               text="알림설정"
@@ -46,7 +46,7 @@
             <v-btn
               size="24"
               icon="custom:setting"
-              @click="isSetting=!isSetting"
+              @click="toggleSetting"
             />
           </template>
         </v-toolbar>
@@ -69,7 +69,7 @@
                 v-for="(item, index) in notifications"
                 :key="index"
               >
-                <v-list-item-title :class="{'dot':item.new}">
+                <v-list-item-title :class="{'dot':item.isNew}">
                   {{ item.title }}
                 </v-list-item-title>
                 <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
@@ -117,7 +117,7 @@
                       v-for="(item, index) in notifications"
                       :key="index"
                     >
-                      <v-list-item-title :class="{'dot':item.new}">
+                      <v-list-item-title :class="{'dot':item.isNew}">
                         {{ item.title }}
                       </v-list-item-title>
                       <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
@@ -137,7 +137,7 @@
                       v-for="(item, index) in notifications"
                       :key="index"
                     >
-                      <v-list-item-title :class="{'dot':item.new}">
+                      <v-list-item-title :class="{'dot':item.isNew}">
                         {{ item.title }}
                       </v-list-item-title>
                       <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
@@ -172,16 +172,18 @@ const notifications = ref([
   {
     title: '알림 내용은 최대 2줄까지 제공 최대 글자 수 초과 시 닷(...) 처리 알림 내용은 최대 2줄까지',
     date: '2025-09-30 09:38',
-    new: true,
+    isNew: true,
   },
   {
     title: '업스토어 신규 앱 출시에 따라 땡땡땡땡 앱 업데이트 버전이 출시되었습니다. 지금 바로',
     date: '2025-09-30 09:38',
-    new: true,
+    isNew: true,
   },
   {
     title: '신규가입이 완료되었습니다.',
     date: '2025-09-30 09:38',
+
+
   },
   {
     title: 'NH QR 앱 사용 신청이 수락되었습니다.',
@@ -212,4 +214,9 @@ const notifications = ref([
     date: '2025-09-30 09:38',
   },
 ]);
+
+// 알림 설정 토글
+const toggleSetting = () => {
+  isSetting.value = !isSetting.value;
+};
 </script>
