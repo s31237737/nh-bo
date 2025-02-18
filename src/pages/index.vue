561ce158d11c0e0ea9de88b9456961c36afedb4c
lim <95602250+s31237737@users.noreply.github.com>
2025-02-18 13:38:41 +0900

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,6 @@
       <v-select
         label="앱 타입"
         :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-
       />
 
       <v-select
@@ -38,7 +37,13 @@
     필터
   </div>
   <div>
-    테이블
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="apps"
+      show-select
+      hide-default-footer
+    />
   </div>
   <Pagination
     :total-items="80"
@@ -54,4 +59,43 @@ const form = ref('');
 const onAppendClick = () => {
   alert('Append icon clicked!');
 }
+
+const selected = ref([]);
+
+const headers = [
+  { title: "순서", key: "id"},
+  { title: "앱 코드", key: "appCode", sortable: false },
+  { title: "앱 이름", key: "appName", sortable: false },
+  { title: "앱 타입", key: "appType", sortable: false },
+  { title: "상태", key: "status", sortable: false },
+  { title: "등록자", key: "registrant", sortable: false },
+  { title: "등록 날짜", key: "createdDate", sortable: false },
+  { title: "업데이트 날짜", key: "updatedDate", sortable: false },
+  { title: "마지막 수정 날짜", key: "lastModifiedDate", sortable: false },
+];
+
+const apps = ref([
+  {
+    id: 1,
+    appCode: "APP001",
+    appName: "테스트 앱 1",
+    appType: "모바일",
+    status: "활성",
+    registrant: "홍길동",
+    createdDate: "2024-01-01",
+    updatedDate: "2024-02-01",
+    lastModifiedDate: "2024-02-15",
+  },
+  {
+    id: 2,
+    appCode: "APP002",
+    appName: "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십",
+    appType: "웹",
+    status: "비활성",
+    registrant: "김철수",
+    createdDate: "2024-01-10",
+    updatedDate: "2024-02-05",
+    lastModifiedDate: "2024-02-16",
+  },
+]);
 </script>
