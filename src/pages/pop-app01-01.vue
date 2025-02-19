f20ca246ca3c12c14a2a3aff092b38c6e082c2cf
s31237737 <s1237737@nate.com>
2025-02-19 21:28:56 +0900

    앱관리 작업중

--- src/pages/pop-app01-01.vue
+++ src/pages/pop-app01-01.vue
@@ -6,6 +6,23 @@
   >
     앱 타입 등록
   </v-btn>
+
+  <v-btn
+    size="small"
+    color="blue"
+    @click="dialog2 = true"
+  >
+    앱 타입 수정
+  </v-btn>
+
+  <v-btn
+    size="small"
+    color="blue"
+    @click="dialog3 = true"
+  >
+    앱 타입 상세
+  </v-btn>
+
   <v-dialog
     v-model="dialog"
     class="popup-md"
@@ -28,12 +45,12 @@
                 앱 타입명 <i class="required" />
               </th>
               <td>
-                <v-text-field />
+                <v-text-field placeholder="앱 타입명을 입력해주세요" />
               </td>
             </tr>
             <tr>
               <th class="w-20">
-                앱 타입 코드 <i class="required" />
+                앱 타입 코드
               </th>
               <td>APP001</td>
             </tr>
@@ -62,19 +79,14 @@
         <v-btn
           color="primary"
           size="large"
+          disabled
         >
           등록
         </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
-  <v-btn
-    size="small"
-    color="blue"
-    @click="dialog2 = true"
-  >
-    앱 타입 수정
-  </v-btn>
+
   <v-dialog
     v-model="dialog2"
     class="popup-md"
@@ -97,12 +109,12 @@
                 앱 타입명 <i class="required" />
               </th>
               <td>
-                <v-text-field />
+                <v-text-field placeholder="앱 타입명을 입력해주세요" />
               </td>
             </tr>
             <tr>
               <th class="w-20">
-                앱 타입 코드 <i class="required" />
+                앱 타입 코드
               </th>
               <td>APP001</td>
             </tr>
@@ -132,6 +144,67 @@
           color="tertiary"
           size="large"
           variant="outlined"
+          prepend-icon="custom:edit"
+        >
+          수정
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog
+    v-model="dialog3"
+    class="popup-md"
+  >
+    <v-card>
+      <v-card-title>
+        <span>앱 타입 상세</span>
+        <v-btn
+          icon="mdi-close"
+          density="comfortable"
+          @click="dialog3 = false"
+        />
+      </v-card-title>
+      <v-card-text>
+        <!-- dialog contents -->
+        <v-table-row>
+          <tbody>
+            <tr
+              v-for="(row, index) in tableData"
+              :key="index"
+            >
+              <th class="w-20">
+                {{ row.label }}
+              </th>
+              <td>{{ row.value }}</td>
+            </tr>
+          </tbody>
+        </v-table-row>
+        <!-- // dialog contents -->
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          color="tertiary"
+          size="large"
+          variant="outlined"
+        >
+          목록
+        </v-btn>
+        <v-spacer />
+        <v-btn
+          color="warning"
+          size="large"
+          variant="outlined"
+          prepend-icon="custom:remove"
+          @click="dialog3 = false"
+        >
+          삭제
+        </v-btn>
+        <v-btn
+          color="tertiary"
+          size="large"
+          variant="outlined"
+          prepend-icon="custom:edit"
         >
           수정
         </v-btn>
@@ -145,6 +218,13 @@ import { ref } from 'vue';
 
 const dialog = ref(true);
 const dialog2 = ref(false);
+const dialog3 = ref(false);
+const tableData = ref([
+  { label: '앱 타입명', value: '대여' },
+  { label: '앱 타입 코드', value: 'APP001' },
+  { label: '사용 여부', value: '사용' },
+]);
+
 const isSwitch = ref(false);
 
 </script>
