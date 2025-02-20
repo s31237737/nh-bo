da8458db9d76a5140d738ebe167316fc97ec0a90
lim <95602250+s31237737@users.noreply.github.com>
2025-02-20 17:34:25 +0900

    Update PopupAppType03.vue

--- src/pages/popup/PopupAppType03.vue
+++ src/pages/popup/PopupAppType03.vue
@@ -8,7 +8,7 @@
       <v-card-title>
         <span>앱 타입 수정</span>
         <v-btn
-          icon="mdi-close"
+          icon="custom:close"
           density="comfortable"
           @click="emit('update:modelValue', false)"
         />
