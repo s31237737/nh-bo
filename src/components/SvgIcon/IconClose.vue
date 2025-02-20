0e3705514cf86e27a2e0f1415e8e636be73c6f87
lim <95602250+s31237737@users.noreply.github.com>
2025-02-20 17:32:21 +0900

    Create IconClose.vue

---
+++ src/components/SvgIcon/IconClose.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10 10.0008L21.8817 21.8825"
+      stroke="#1C1C1C"
+      stroke-width="1.6"
+      stroke-linecap="round"
+    />
+    <path
+      d="M10.1184 21.8817L22.0002 10"
+      stroke="#1C1C1C"
+      stroke-width="1.6"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
