e35457a0030447f0d08f227f4f338deb1b944bb5
lim <95602250+s31237737@users.noreply.github.com>
2025-02-21 17:38:21 +0900

    Create Upload.vue

---
+++ src/components/Upload.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="file-upload">
+    <v-file-upload
+      v-model="model"
+      clearable
+      multiple
+      icon="mdi-plus"
+    >
+      <template #item="{ props: itemProps }">
+        <v-file-upload-item
+          v-bind="itemProps"
+          file-icon="mdi-plus"
+        >
+          <template #clear="{ props: clearProps }">
+            <v-btn
+              color="primary"
+              v-bind="clearProps"
+            />
+          </template>
+        </v-file-upload-item>
+      </template>
+    </v-file-upload>
+  </div>
+</template>
+<script setup>
+  import { shallowRef } from 'vue'
+
+  const model = shallowRef(null)
+</script>
