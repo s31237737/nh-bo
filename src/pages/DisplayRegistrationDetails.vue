a51d87a9ac11e7a06f096f0ca80b591c933af74c
s31237737 <s1237737@nate.com>
2025-02-23 14:41:57 +0900

    수정

--- src/pages/DisplayRegistrationDetails.vue
+++ src/pages/DisplayRegistrationDetails.vue
@@ -1,48 +1,98 @@
 <template>
-  <PageHeader title="콘텐츠 상세" prev-link="DisplayRegistration" />
+  <PageHeader
+    title="콘텐츠 상세"
+    prev-link="DisplayRegistration"
+  />
 
   <div class="page-contents">
     <v-table-row>
       <tbody>
         <tr>
-          <th style="width: 10%">콘텐츠 유형</th>
-          <td style="width: 90%">이미지</td>
-        </tr>
-        <tr>
-          <th style="width: 10%">콘텐츠 제목</th>
-          <td style="width: 90%">육묘대장</td>
-        </tr>
-        <tr>
-          <th style="width: 10%">배너 제목</th>
-          <td style="width: 90%">육묘대장</td>
-        </tr>
-        <tr>
-          <th style="width: 10%">바로가기 링크</th>
+          <th style="width: 10%">
+            콘텐츠 유형
+          </th>
           <td style="width: 90%">
-            <a href="https://nonghyup.com" target="_blank"
-              >https://nonghyup.com</a
-            >
+            이미지
           </td>
         </tr>
         <tr>
-          <th style="width: 10%">이미지</th>
+          <th style="width: 10%">
+            콘텐츠 제목
+          </th>
           <td style="width: 90%">
-            <ImageUploader :images="uploaderList" readonly />
+            육묘대장
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%">
+            배너 제목
+          </th>
+          <td style="width: 90%">
+            육묘대장
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%">
+            바로가기 링크
+          </th>
+          <td style="width: 90%">
+            <a
+              href="https://nonghyup.com"
+              target="_blank"
+            >https://nonghyup.com</a>
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%">
+            이미지
+          </th>
+          <td style="width: 90%">
+            <ImageUploader
+              :images="uploaderList"
+              readonly
+            />
             <div class="upload-guide">
               <p>1240px X 140px</p>
             </div>
           </td>
         </tr>
         <tr>
-          <th style="width: 10%">노출 기간</th>
-          <td style="width: 90%">2025.01.21 ~ 2025.01.30</td>
+          <th style="width: 10%">
+            노출 기간
+          </th>
+          <td style="width: 90%">
+            2025.01.21 ~ 2025.01.30
+          </td>
         </tr>
         <tr>
-          <th style="width: 10%">사용 상태</th>
-          <td style="width: 90%">미사용</td>
+          <th style="width: 10%">
+            사용 상태
+          </th>
+          <td style="width: 90%">
+            미사용
+          </td>
         </tr>
       </tbody>
     </v-table-row>
+  </div>
+  <div class="page-actions">
+    <v-btn
+      size="large"
+      color="warning"
+      variant="outlined"
+      prepend-icon="custom:remove"
+    >
+      삭제
+    </v-btn>
+    <v-btn
+      color="tertiary"
+      size="large"
+      variant="outlined"
+      prepend-icon="custom:edit"
+      :to="'/AppRegistration'"
+    >
+      수정
+    </v-btn>
   </div>
 </template>
 
