6344ecffdbdc7488798d803851ba10b3d3c030fa
lim <95602250+s31237737@users.noreply.github.com>
2025-02-24 11:07:44 +0900

    Update IconRadioOff.vue

--- src/components/SvgIcon/IconRadioOff.vue
+++ src/components/SvgIcon/IconRadioOff.vue
@@ -1,15 +1,15 @@
 <template>
   <svg
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#Radiobutton_clip0-11-57)">
+    <g clip-path="url(#Frame3_clip0-13-72)">
       <circle
-        cx="10"
-        cy="10"
+        cx="12"
+        cy="12"
         r="9"
         fill="#FEFEFE"
         stroke="#E5E5E5"
@@ -17,8 +17,10 @@
       />
     </g>
     <defs>
-      <clipPath id="Radiobutton_clip0-11-57">
+      <clipPath id="Frame3_clip0-13-72">
         <rect
+          x="2"
+          y="2"
           width="20"
           height="20"
           rx="4"
