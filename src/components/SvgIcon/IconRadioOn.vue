298b6c43a106199beaaccc9d6727ec1c2cfc17eb
lim <95602250+s31237737@users.noreply.github.com>
2025-02-24 10:48:49 +0900

    Create IconRadioOn.vue

---
+++ src/components/SvgIcon/IconRadioOn.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#Radiobutton_clip0-11-58)">
+      <circle
+        cx="10"
+        cy="10"
+        r="9"
+        fill="#FEFEFE"
+        stroke="#1C1C1C"
+        stroke-width="2"
+      />
+      <circle
+        cx="10"
+        cy="10"
+        r="6"
+        fill="#1C1C1C"
+      />
+    </g>
+    <defs>
+      <clipPath id="Radiobutton_clip0-11-58">
+        <rect
+          width="20"
+          height="20"
+          rx="4"
+          fill="white"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
