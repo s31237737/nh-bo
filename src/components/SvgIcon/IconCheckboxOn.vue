5386a656df12018aee4b29c39148613cce515347
lim <95602250+s31237737@users.noreply.github.com>
2025-02-24 10:49:22 +0900

    Create IconCheckboxOn.vue

---
+++ src/components/SvgIcon/IconCheckboxOn.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      width="20"
+      height="20"
+      rx="4"
+      fill="#1C1C1C"
+    />
+    <path
+      d="M14.3333 7L7.91667 13.4167L5 10.5"
+      stroke="#FEFEFE"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
