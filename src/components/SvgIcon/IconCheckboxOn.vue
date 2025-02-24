3ee8c921d978e3130940b671876d627bbe9e726f
lim <95602250+s31237737@users.noreply.github.com>
2025-02-24 11:07:30 +0900

    Update IconCheckboxOn.vue

--- src/components/SvgIcon/IconCheckboxOn.vue
+++ src/components/SvgIcon/IconCheckboxOn.vue
@@ -1,18 +1,21 @@
 <template>
   <svg
-    width="20"
-    height="20"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <rect
+      x="2"
+      y="1.5"
       width="20"
-      height="20"
+      height="21"
       rx="4"
       fill="#1C1C1C"
     />
     <path
-      d="M14.3333 7L7.91667 13.4167L5 10.5"
+      d="M16.3333 9.5L9.91667 15.9167L7 13"
       stroke="#FEFEFE"
       stroke-width="2"
       stroke-linecap="round"
