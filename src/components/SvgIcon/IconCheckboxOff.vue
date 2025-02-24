9f5ce99133d9ccbc75314a01b7c99a9f0e63f382
lim <95602250+s31237737@users.noreply.github.com>
2025-02-24 11:07:11 +0900

    Update IconCheckboxOff.vue

--- src/components/SvgIcon/IconCheckboxOff.vue
+++ src/components/SvgIcon/IconCheckboxOff.vue
@@ -1,21 +1,23 @@
 <template>
   <svg
-    width="20"
-    height="20"
+    width="24"
+    height="24"
+    style=""
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <rect
-      x="0.5"
-      y="0.5"
+      x="2.5"
+      y="2.5"
       width="19"
       height="19"
       rx="3.5"
       fill="#FAFAFA"
     />
     <rect
-      x="0.5"
-      y="0.5"
+      x="2.5"
+      y="2.5"
       width="19"
       height="19"
       rx="3.5"
