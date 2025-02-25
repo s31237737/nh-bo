17eed382b132a8e388e82383ab9fd0e52f0015cd
lim <95602250+s31237737@users.noreply.github.com>
2025-02-25 17:39:14 +0900

    Update QnaRegistration.vue

--- src/pages/QnaRegistration.vue
+++ src/pages/QnaRegistration.vue
@@ -1,56 +1,139 @@
 <template>
   <PageHeader
-    title="Q&A 등록"
+    title="Q&A 답변 등록"
     prev-link="Qna"
   />
   <div class="page-contents">
+    <!-- 문의내용 -->
+    <div class="tit-wrap">
+      <strong class="title-2">
+        문의 내용
+      </strong>
+    </div>
     <v-table-row>
       <tbody>
-        <tr>
-          <th style="width: 10%;">
-            제목 <i class="required" />
-          </th>
-          <td colspan="7">
-            <v-text-field placeholder="제목을 입력해주세요." />
-          </td>
-        </tr>
         <tr>
           <th style="width: 10%">
             등록자
           </th>
-          <td style="width: 15%">
+          <td style="width: 23%">
             김농협(05300300)
           </td>
           <th style="width: 10%">
-            상단 고정 여부
+            공개여부
           </th>
-          <td style="width: 15%">
-            <v-switch
-              v-model="isSwitch01"
-            />
+          <td style="width: 23%">
+            공개
           </td>
           <th style="width: 10%">
-            공개 여부
+            답변상태
           </th>
-          <td style="width: 15%">
-            <v-switch
-              v-model="isSwitch02"
-            />
+          <td style="width: 24%">
+            미답변
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%">
+            휴대전화
+          </th>
+          <td style="width: 23%">
+            010-0000-0000
           </td>
           <th style="width: 10%">
-            메인 노출 여부
+            이메일
           </th>
-          <td style="width: 15%">
-            <v-switch
-              v-model="isSwitch03"
-            />
+          <td style="width: 23%">
+            Nonghyup@gmail.com
+          </td>
+          <th style="width: 10%">
+            작성일
+          </th>
+          <td style="width: 24%">
+            YYYY.MM.DD.hh.mm
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%">
+            문의유형
+          </th>
+          <td style="width: 23%">
+            앱 문의
+          </td>
+          <th style="width: 10%">
+            앱
+          </th>
+          <td colspan="3">
+            육묘대장
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%;">
+            제목
+          </th>
+          <td colspan="5">
+            Q&A 질문 제목입니다.
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%;">
+            내용
+          </th>
+          <td colspan="5">
+            <p>
+              안녕하세요.<br>
+              앱 사용할때 궁금한점이 있어 문의드립니다.<br>
+              Q&A 내용은 여기에 적으면 되나요?<br>
+              답변 부탁드립니다.<br>
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <th style="width: 10%;">
+            첨부파일
+          </th>
+          <td colspan="5">
+            <ul
+              class="file-list"
+            >
+              <li
+                v-for="(file, index) in files"
+                :key="index"
+              >
+                <v-icon icon="custom:file" />
+                <span>{{ file.name }}</span>
+                <em>{{ file.size }}</em>
+                <v-btn
+                  class="icon-md"
+                  icon="custom:download"
+                  @click="downloadFile(file)"
+                />
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </v-table-row>
+    <!-- 답변 -->
+    <div class="tit-wrap">
+      <strong class="title-2">
+        답변
+      </strong>
+    </div>
+    <v-table-row>
+      <tbody>
+        <tr>
+          <th style="width: 10%;">
+            답변자
+          </th>
+          <td>
+            관리자
           </td>
         </tr>
         <tr>
           <th style="width: 10%;">
             내용 <i class="required" />
           </th>
-          <td colspan="7">
+          <td>
             <v-sheet
               class="d-flex align-center justify-center"
               color="secondary"
@@ -65,9 +148,9 @@
           <th style="width: 10%;">
             첨부파일
           </th>
-          <td colspan="7">
+          <td>
             <v-file-input
-              v-model="files"
+              v-model="files02"
               placeholder="글제목, 등록자를 검색해주세요."
               multiple
             >
@@ -104,27 +187,6 @@
         </tr>
       </tbody>
     </v-table-row>
-    <!-- 토스트 팝업 -->
-    <v-snackbar
-      v-model="snackbar"
-      contained
-      content-class="toast-pop"
-    >
-      <v-icon
-        start
-        size="24"
-        icon="custom:alert"
-      />
-      {{ snackbarText }}
-      <template #actions>
-        <v-btn
-          icon="custom:close"
-          density="comfortable"
-          @click="snackbar = false"
-        />
-      </template>
-    </v-snackbar>
-    <!-- // 토스트 팝업 -->
   </div>
   <div class="page-actions">
     <v-btn
@@ -133,20 +195,28 @@
     >
       취소
     </v-btn>
+    <v-btn
+      size="large"
+      color="warning"
+      variant="outlined"
+      prepend-icon="custom:remove"
+    >
+      삭제
+    </v-btn>
     <!-- <v-btn
       color="tertiary"
       size="large"
       variant="outlined"
       prepend-icon="custom:edit"
     >
-      수정
+      답변 수정
     </v-btn> -->
     <v-btn
       color="primary"
       size="large"
-      @click="snackbarOpen"
+      disabled
     >
-      등록
+      답변 등록
     </v-btn>
   </div>
 </template>
@@ -154,19 +224,18 @@
 <script setup>
 import { ref } from 'vue';
 
-const isSwitch01 = ref(false);
-const isSwitch02 = ref(true);
-const isSwitch03 = ref(false);
+const files = ref([
+  { name: "C:/Download/Filename.png", size: "10.3MB", url: "#" },
+  { name: "C:/Download/Filename123.png", size: "10.3MB", url: "#" },
+  { name: "C:/Download/Filename456.png", size: "10.3MB", url: "#" },
+]);
 
-const snackbar = ref(false);
-const snackbarText = ref('메인 공지사항은 5개까지 노출이 가능합니다. 노출 개수를 확인하고 다시 설정해 주세요.');
-const snackbarOpen = () => {
-  snackbar.value=true
-    return;
-}
+const downloadFile = (files) => {
+  console.log(files.name)
+};
 
-const files = ref([]);
+const files02 = ref([]);
 const removeFile = (index) => {
-  files.value.splice(index, 1);
+  files02.value.splice(index, 1);
 };
 </script>
