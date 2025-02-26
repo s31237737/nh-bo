d96d5e48204909603bf01da962bff0e9d71cb741
lim <95602250+s31237737@users.noreply.github.com>
2025-02-26 11:13:48 +0900

    Update DisplayRegistration01.vue

--- src/pages/DisplayRegistration01.vue
+++ src/pages/DisplayRegistration01.vue
@@ -16,239 +16,226 @@
             colspan="4"
           >
             <v-select
-              v-model="select"
-              :items="['앱 추천']"
+              v-model="selectedType"
+              :items="['앱 추천','이미지']"
               style="width: 640px"
             />
           </td>
         </tr>
-        <tr>
-          <th style="width: 10%">
-            콘텐츠 제목 <i class="required" />
-          </th>
-          <td
-            style="width: 90%"
-            colspan="4"
-          >
-            <v-text-field
-              placeholder="콘텐츠 제목을 입력해주세요."
-              style="width: 640px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            배너 제목 <i class="required" />
-          </th>
-          <td
-            style="width: 90%"
-            colspan="4"
-          >
-            <v-text-field
-              placeholder="배너 제목을 입력해주세요."
-              style="width: 640px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            간단 설명 <i class="required" />
-          </th>
-          <td colspan="4">
-            <v-textarea
-              placeholder="간단 설명을 입력해주세요."
-              :counter="30"
-              :persistent-counter="true"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th
-            style="width: 10%"
-            rowspan="3"
-          >
-            앱 정보 <i class="required" />
-          </th>
-          <th
-            class="th-sub"
-            style="width: 10%"
-          >
-            앱 검색
-          </th>
-          <td style="width: 30%">
-            <v-btn color="primary">
+        <template v-if="selectedType === '앱 추천'">
+          <tr>
+            <th style="width: 10%">
+              콘텐츠 제목 <i class="required" />
+            </th>
+            <td
+              style="width: 90%"
+              colspan="4"
+            >
+              <v-text-field
+                placeholder="콘텐츠 제목을 입력해주세요."
+                style="width: 640px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              배너 제목 <i class="required" />
+            </th>
+            <td
+              style="width: 90%"
+              colspan="4"
+            >
+              <v-text-field
+                placeholder="배너 제목을 입력해주세요."
+                style="width: 640px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              간단 설명 <i class="required" />
+            </th>
+            <td colspan="4">
+              <v-textarea
+                placeholder="간단 설명을 입력해주세요."
+                :counter="30"
+                :persistent-counter="true"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th
+              style="width: 10%"
+              rowspan="3"
+            >
+              앱 정보 <i class="required" />
+            </th>
+            <th
+              class="th-sub"
+              style="width: 10%"
+            >
               앱 검색
-            </v-btn>
-          </td>
-          <th
-            style="width: 10%"
-            rowspan="3"
-          >
-            앱 아이콘
-          </th>
-          <td
-            rowspan="3"
-            style="width: 40%"
-          >
-            <ImageUploader
-              :images="uploaderList"
-              readonly
-            />
-            <div class="upload-guide">
-              <p>100px X 100px</p>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <th
-            class="th-sub"
-            style="width: 10%"
-          >
-            앱 이름
-          </th>
-          <td>육모대장</td>
-        </tr>
-        <tr>
-          <th
-            class="th-sub"
-            style="width: 10%"
-          >
-            앱 타입
-          </th>
-          <td>대여</td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            바로가기 링크
-          </th>
-          <td colspan="4">
-            <v-text-field
-              placeholder="바로가기 링크를 입력해주세요."
-              style="width: 640px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            노출 기간 <i class="required" />
-          </th>
-          <td colspan="4">
-            <DateRange
-              v-model="dateRange"
-              style="width: 340px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            사용 상태
-          </th>
-          <td colspan="4">
-            <v-switch
-              v-model="isSwitch"
-              @change="snackbarOpen"
-            />
-          </td>
-        </tr>
-      </tbody>
-    </v-table-row>
-    <!-- 콘텐츠 유형 : 이미지 -->
-    <v-table-row>
-      <tbody>
-        <tr>
-          <th style="width: 10%">
-            콘텐츠 유형 <i class="required" />
-          </th>
-          <td style="width: 90%">
-            <v-select
-              v-model="select2"
-              :items="['이미지']"
-              style="width: 640px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            콘텐츠 제목
-          </th>
-          <td style="width: 90%">
-            <v-text-field
-              placeholder="콘텐츠 제목을 입력해주세요."
-              style="width: 640px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            배너 제목
-          </th>
-          <td style="width: 90%">
-            <v-text-field
-              placeholder="배너 제목을 입력해주세요."
-              style="width: 640px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            간단 설명
-          </th>
-          <td>
-            <v-textarea
-              placeholder="간단 설명을 입력해주세요."
-              :counter="30"
-              :persistent-counter="true"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            이미지 <i class="required" />
-          </th>
-          <td style="width: 90%">
-            <ImageUploader
-              :images="appIconList"
-              @update:images="updateAppIconList"
-            />
-            <div class="upload-guide">
-              <p>권장크기: 930px x 258px</p>
-              <p>용량 : 10MB 이하, 파일형식 JPG,PNG</p>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            바로가기 링크
-          </th>
-          <td>
-            <v-text-field
-              placeholder="앱 바로가기 링크를 입력해주세요."
-              style="width: 640px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            노출 기간 <i class="required" />
-          </th>
-          <td>
-            <DateRange
-              v-model="dateRange"
-              style="width: 340px"
-            />
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            사용 상태
-          </th>
-          <td>
-            <v-switch
-              v-model="isSwitch"
-              @change="snackbarOpen"
-            />
-          </td>
-        </tr>
+            </th>
+            <td style="width: 30%">
+              <v-btn color="primary">
+                앱 검색
+              </v-btn>
+            </td>
+            <th
+              style="width: 10%"
+              rowspan="3"
+            >
+              앱 아이콘
+            </th>
+            <td
+              rowspan="3"
+              style="width: 40%"
+            >
+              <ImageUploader
+                :images="uploaderList"
+                readonly
+              />
+              <div class="upload-guide">
+                <p>100px X 100px</p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th
+              class="th-sub"
+              style="width: 10%"
+            >
+              앱 이름
+            </th>
+            <td>육모대장</td>
+          </tr>
+          <tr>
+            <th
+              class="th-sub"
+              style="width: 10%"
+            >
+              앱 타입
+            </th>
+            <td>대여</td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              바로가기 링크
+            </th>
+            <td colspan="4">
+              <v-text-field
+                placeholder="바로가기 링크를 입력해주세요."
+                style="width: 640px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              노출 기간 <i class="required" />
+            </th>
+            <td colspan="4">
+              <DateRange
+                v-model="dateRange"
+                style="width: 340px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              사용 상태
+            </th>
+            <td colspan="4">
+              <v-switch
+                v-model="isSwitch01"
+                @change="snackbarOpen"
+              />
+            </td>
+          </tr>
+        </template>
+        <template v-else>
+          <tr>
+            <th style="width: 10%">
+              콘텐츠 제목
+            </th>
+            <td style="width: 90%">
+              <v-text-field
+                placeholder="콘텐츠 제목을 입력해주세요."
+                style="width: 640px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              배너 제목
+            </th>
+            <td style="width: 90%">
+              <v-text-field
+                placeholder="배너 제목을 입력해주세요."
+                style="width: 640px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              간단 설명
+            </th>
+            <td>
+              <v-textarea
+                placeholder="간단 설명을 입력해주세요."
+                :counter="30"
+                :persistent-counter="true"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              이미지 <i class="required" />
+            </th>
+            <td style="width: 90%">
+              <ImageUploader
+                :images="appIconList"
+                @update:images="updateAppIconList"
+              />
+              <div class="upload-guide">
+                <p>권장크기: 930px x 258px</p>
+                <p>용량 : 10MB 이하, 파일형식 JPG,PNG</p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              바로가기 링크
+            </th>
+            <td>
+              <v-text-field
+                placeholder="앱 바로가기 링크를 입력해주세요."
+                style="width: 640px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              노출 기간 <i class="required" />
+            </th>
+            <td>
+              <DateRange
+                v-model="dateRange"
+                style="width: 340px"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th style="width: 10%">
+              사용 상태
+            </th>
+            <td>
+              <v-switch
+                v-model="isSwitch02"
+                @change="snackbarOpen"
+              />
+            </td>
+          </tr>
+        </template>
       </tbody>
     </v-table-row>
     <!-- 토스트 팝업 -->
@@ -319,10 +306,10 @@ const uploaderList = ref([
 ]);
 
 // 사용 상태
-const isSwitch = ref(false);
+const isSwitch01 = ref(false);
+const isSwitch02 = ref(false);
 
-const select = ref("앱 추천");
-const select2 = ref("이미지");
+const selectedType = ref("앱 추천");
 
 const dateRange = ref([new Date(), new Date()]);
 </script>
