1f4662bb1083d5cfd9e015237729344433eedf10
lim <95602250+s31237737@users.noreply.github.com>
2025-02-26 11:13:34 +0900

    Update DisplayDetails01.vue

--- src/pages/DisplayDetails01.vue
+++ src/pages/DisplayDetails01.vue
@@ -1,7 +1,7 @@
 <template>
   <PageHeader
     title="콘텐츠 상세"
-    prev-link="DisplayRegistration01"
+    prev-link="Display01"
   />
 
   <div class="page-contents">
@@ -125,85 +125,6 @@
             style="width: 90%"
             colspan="4"
           >
-            미사용
-          </td>
-        </tr>
-      </tbody>
-    </v-table-row>
-    <!-- 콘텐츠 유형 : 이미지 -->
-    <v-table-row>
-      <tbody>
-        <tr>
-          <th style="width: 10%">
-            콘텐츠 유형
-          </th>
-          <td style="width: 90%">
-            이미지
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            콘텐츠 제목
-          </th>
-          <td style="width: 90%">
-            육묘대장
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            배너 제목
-          </th>
-          <td style="width: 90%">
-            육묘대장
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            간단 설명
-          </th>
-          <td style="width: 90%">
-            간편하고 편리한 '육묘대장'으로 손쉽게 관리 하실 수 있습니다.
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            이미지
-          </th>
-          <td style="width: 90%">
-            <ImageUploader
-              class="type02"
-              :images="uploaderList"
-              readonly
-            />
-            <div class="upload-guide">
-              <p>930px X 258px</p>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            바로가기 링크
-          </th>
-          <td style="width: 90%">
-            <a
-              href="https://nonghyup.com"
-              target="_blank"
-            >https://nonghyup.com</a>
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            노출 기간
-          </th>
-          <td style="width: 90%">
-            2025.01.21 ~ 2025.01.30
-          </td>
-        </tr>
-        <tr>
-          <th style="width: 10%">
-            사용 상태
-          </th>
-          <td style="width: 90%">
             미사용
           </td>
         </tr>
