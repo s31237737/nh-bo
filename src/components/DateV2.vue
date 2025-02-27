5534bf7a26da25e7982d5697a300d44dc59dd797
lim <95602250+s31237737@users.noreply.github.com>
2025-02-27 17:18:51 +0900

    Create DateV2.vue

---
+++ src/components/DateV2.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-menu
+    v-model="menu"
+    location="center"
+  >
+    <template #activator="{ props }">
+      <v-text-field
+        v-model="date"
+        prepend-inner-icon="mdi-calendar"
+        readonly
+        v-bind="props"
+      />
+    </template>
+    <div class="position-relative">
+      <v-date-picker
+        v-model="date"
+        height="auto"
+        show-adjacent-months
+      />
+      <v-btn
+        class="position-absolute top-0 right-0"
+        color="white"
+        icon="mdi-close"
+        size="small"
+        variant="text"
+        @click="menu = false"
+      />
+    </div>
+  </v-menu>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const menu = ref(false);
+const date = ref();
+</script>
